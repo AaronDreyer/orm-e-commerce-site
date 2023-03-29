69b2737739c8e98d3864633d70887c81dc33b713
AaronDreyer <aaronottaway@gmail.com>
2023-03-29 14:30:50 -0400

    Models and ENV
    Ive added the appropriate columns to each model according to acceptance criteria and structure provided in the challenge detail. Ive also added an env file for password security.

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -9,7 +9,37 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      validate: {
+        isDecimal: true,
+      },
+      allowNull: false,
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      },
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category'
+      },
+    },
   },
   {
     sequelize,
@@ -21,3 +51,45 @@ Product.init(
 );
 
 module.exports = Product;
+
+// Product
+
+// id
+
+// Integer
+
+// Doesn't allow null values
+
+// Set as primary key
+
+// Uses auto increment
+
+    // product_name
+
+// String
+
+// Doesn't allow null values
+
+    // price
+
+// Decimal
+
+// Doesn't allow null values
+
+// Validates that the value is a decimal
+
+    // stock
+
+// Integer
+
+// Doesn't allow null values
+
+// Set a default value of 10
+
+// Validates that the value is numeric
+
+    // category_id
+
+// Integer
+
+// References the category model's id
--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -6,7 +6,24 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'product',
+      },
+    },
+    tag_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'tag',
+      },
+    },
   },
   {
     sequelize,
@@ -18,3 +35,27 @@ ProductTag.init(
 );
 
 module.exports = ProductTag;
+
+ProductTag
+
+    // id
+
+// Integer
+
+// Doesn't allow null values
+
+// Set as primary key
+
+// Uses auto increment
+
+    // product_id
+
+// Integer
+
+// References the product model's id
+
+    // tag_id
+
+// Integer
+
+// References the tag model's id
