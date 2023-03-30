1f5a91aaa705d0c71f83616a29b961d392a45145
AaronDreyer <aaronottaway@gmail.com>
2023-03-29 21:27:25 -0400

    Through within Tag and Refactoring code
    I found where I was having an issue with my tag routes. I forgot to add the through based on the Product Tag within the index.js model. Im not sure if theres a portion within the product route to add a through but product routes on insomnia worked just fine. I also refactored the code within the product route file from the starter code to code that match my restful route code structure ive been using.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -37,10 +37,10 @@ router.get('/:id', async (req, res) => {
 router.post('/', async (req, res) => {
   // create a new category
   try {
-    const locationData = await Category.create({
+    const categoryData = await Category.create({
       product_id: req.body.product_id,
     });
-    res.status(200).json(locationData);
+    res.status(200).json(categoryData);
   } catch (err) {
     res.status(400).json(err);
   }
